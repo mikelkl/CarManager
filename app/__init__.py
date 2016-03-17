d9c5fd78c68e0a385e338c0b8424e421c1c01030
Mike Liu <mikelkl@foxmail.com>
2016-03-17 10:57:52 +0800

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from config import config
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+
+    from .api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
+    return app
